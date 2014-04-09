d655b07135bbedba343479ad65f9bc43ffc1b7f6
Arron Schaar <aschaar@gmail.com>
2014-04-09 13:24:09 -0700

    update dependencies, added new grunt tasks for easy bump and push to gh-pages

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,6 +27,14 @@ module.exports = function(grunt) {
       file: 'package.json',
       prefix: '',
       commit: true
+    },
+    exec: {
+      'update_gh_pages':{
+        cmd: 'git stash && git checkout gh-pages && git rebase master && git push origin gh-pages && git checkout master && git stash pop'
+      },
+      'update_master':{
+        cmd: 'git push origin master --tags'
+      }
     }
   });
 
@@ -36,8 +44,11 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-bumpup');
   grunt.loadNpmTasks('grunt-tagrelease');
   grunt.loadNpmTasks('grunt-smush-components');
+  grunt.loadNpmTasks('grunt-exec');
 
   grunt.registerTask('build', ['jshint','smush-components']);
   grunt.registerTask('bump:patch', ['bumpup:patch', 'tagrelease']);
+  grunt.registerTask('push', ['exec:update_master','exec:update_gh_pages']);
+  grunt.registerTask('bump-push', ['bump:patch','push'])
 
 };
